60c3b8768a3dcc70d4b3ccf69643487552a6ef77
Nimesh <nimeshvishwav@gmail.com>
2021-12-22 10:41:46 +0530

    asdf

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // navbar scroll animation
   $(window).scroll(function () {
     if ($(document).scrollTop()>50){
       $('.navbar').addClass('scroll')
@@ -10,5 +11,40 @@ $(document).ready(function () {
       $('.nav-link').addClass('scroll')
     };
   });
+
+  //typing animation script
+  var typed = new Typed('.animate', {
+    strings: [
+      "Developer",
+      "Designer",
+      "Progammer",
+      "Gamer"
+    ],
+    typeSpeed:100,
+    backSpeed: 80,
+    loop: true
+  });
+
+  var typed = new Typed('.animate-2', {
+    strings: [
+      "Developer",
+      "Designer",
+      "Progammer",
+      "Gamer"
+    ],
+    typeSpeed:100,
+    backSpeed: 80,
+    loop: true
+  });
+
+  var typed = new Typed('.testing', {
+    strings: [
+      "progress",
+    ],
+    typeSpeed:100,
+    backSpeed: 40,
+    loop: true
+  });
+
 });
 
