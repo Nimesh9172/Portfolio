be8b04058d337dea8a33332c2ce37c45f6c42e6b
Ravikant Vishwakarma <ravikant.vishwakarma@techprescient.com>
2022-10-09 12:42:28 +0530

    a

--- js/main.js
+++ js/main.js
@@ -91,3 +91,15 @@ const handleResize = () => {
 
 window.addEventListener("resize",handleResize);
 // matrix raining effect  end
+
+
+const btn = document.querySelector('.ripple');
+const btn2 = document.querySelector('.ripple2');
+btn.onmousemove = function (e) {
+  const x = e.pageX - btn.offsetLeft
+  const y = e.pageY - btn.offsetTop
+
+  btn.style.setProperty('--x',x + 'px')
+  btn.style.setProperty('--y',y + 'px')
+}
+
